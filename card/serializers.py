fc40f7dd63c2f759955dadbf0fb4c448be73a02d
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-09-14 07:59:30 +0500

    update user

---
+++ card/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Card, CardItem
+
+class CardItemSerializer(serializers.ModelSerializer):
+    total_price = serializers.ReadOnlyField()
+    class Meta:
+        model = CardItem
+        fields = ['id', 'card', 'product', 'ammount', 'created_at', 'updated_at', 'total_price']
+
+class CardSerializer(serializers.ModelSerializer):
+    items =  CardItemSerializer(many=True, read_only=True)
+    total_price = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Card
+        fields = ['id', 'user', 'created_at', 'total_price', 'items']
--- card/views.py
+++ card/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from products.models import Product
+from .serializers import CardSerializer, CardItemSerializer
+from .models import Card, CardItem
+from user_acc.user_perm import IsUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
